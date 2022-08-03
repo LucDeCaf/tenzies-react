559c6345541c19d2ad66d6031e80300f3996ba59
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-08-03 07:39:39 +0200

    created dice component + added placeholders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Dice from "./components/Dice.js";
 
 function App() {
   return (
     <main>
-      
+      <div className="dice-container">
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+        <Dice className="dice" value={Math.ceil(Math.random() * 6)} />
+      </div>
     </main>
   );
 }
---
+++ src/components/Dice.js
@@ -0,0 +1,9 @@
+function Dice(props) {
+  return (
+    <div className="dice">
+      <h2>{props.value}</h2>
+    </div>
+  )
+}
+
+export default Dice;
