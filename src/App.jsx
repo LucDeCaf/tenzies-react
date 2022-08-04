a58e9fd207cace84c8e4fbe1c98eb4db4109547d
Luc de Cafmeyer <ldecafmeyer@gmail.com>
2022-08-04 18:44:47 +0200

    Added nanoid + updated dice and reroll button stylings

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,37 @@ import { useState } from "react";
 import "./App.css";
 import Dice from "./components/Dice";
 import Reroll from "./components/Reroll";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [diceValues, setDiceValues] = useState(allNewDice());
-  const diceElements = diceValues.map(value => <Dice className="dice" value={value} />);
+  const [dice, setDice] = useState(allNewDice());
+  const diceElements = dice.map((dice) => (
+    <Dice key={nanoid()} value={dice.value} isHeld={dice.isHeld} />
+  ));
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: true,
+      });
     }
     return newDice;
   }
 
   function rollDice() {
-    setDiceValues(allNewDice());
+    setDice(allNewDice());
   }
 
   return (
     <main>
       <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className="dice-container">
-        {diceElements}
-      </div>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
       <Reroll handleClick={rollDice} />
     </main>
   );
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,6 +1,6 @@
 export default function Dice(props) {
   return (
-    <div className="dice">
+    <div className={props.isHeld ? "dice dice--held" : "dice"}>
       <h2>{props.value}</h2>
     </div>
   )
